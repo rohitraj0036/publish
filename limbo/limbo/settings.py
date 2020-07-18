0822d2b6f3e52b418cc54cb713ea20558a2c37c8
rohitraj0036 <rohitkr0036@gmail.com>
2020-07-18 14:02:19 +0000

    media

--- limbo/limbo/settings.py
+++ limbo/limbo/settings.py
@@ -125,5 +125,5 @@ STATIC_ROOT = STATIC_DIR = os.path.join(BASE_DIR, "affiliate/static")
 STATICFILE_DIR = [
     STATIC_DIR
 ]
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/image/'
+MEDIA_ROOT= os.path.join(BASE_DIR)
+MEDIA_URL = '/media/'
